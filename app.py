6ce5c3fc677c6cb0ba9be179f84ac169afa7e782
omair <omair.afzal@mission.dev>
2022-07-02 03:27:58 +0500

    Gunicorn added

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def pull_places():
     return get_places(request.values.get('cursor'), places)
 
 
-# if __name__ == '__main__':
-#     app.run(load_dotenv=True)
+if __name__ == '__main__':
+    app.run(load_dotenv=True)
